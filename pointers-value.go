3579c36cee645613a9e0114f420fccfa0bfec833
Kammojr7 <86921322+Kammojr7@users.noreply.github.com>
2021-08-15 12:39:29 +0530

    Create pointers-value.go

---
+++ pointers-value.go
@@ -0,0 +1,34 @@
+package main
+  
+import "fmt"
+  
+type author struct {
+    name   string
+    branch string
+}
+  
+func (a *author) show_1(abranch string) {
+    (*a).branch = abranch
+}
+  
+func (a author) show_2() {
+  
+    a.name = "Kamlesh"
+    fmt.Println("Author's name(Before) : ", a.name)
+}
+  
+func main() {
+  
+    res := author{
+        name:   "Bhawna",
+        branch: "CSE",
+    }
+  
+    fmt.Println("Branch Name(Before): ", res.branch)
+  
+    res.show_1("ECE")
+    fmt.Println("Branch Name(After): ", res.branch)
+  
+    (&res).show_2()
+    fmt.Println("Author's name(After): ", res.name)
+}
